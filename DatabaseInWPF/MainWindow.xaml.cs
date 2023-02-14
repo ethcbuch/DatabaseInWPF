0b1304156f829be0d0e571a8119eab4a7e3359b7
Ethan Buchanan <ethcbuch@ut.utm.edu>
2023-02-14 14:12:15 -0600

    heh

--- DatabaseInWPF/MainWindow.xaml.cs
+++ DatabaseInWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +23,32 @@ namespace DatabaseInWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
         public MainWindow()
         {
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\CW4.accdb");
             InitializeComponent();
         }
+
+        public void Do_assetsButton_Click(object sender, RoutedEventArgs e, OleDbConnection cq)
+        {
+            string query = "select *from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cq);
+            cq.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read()) 
+            {
+                data += read[0].ToString() + "\n";
+            }
+
+            AssetsText.Text = data;
+        }
+
+        private void assetsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Do_assetsButton_Click(sender, e, cn);
+        }
+
     }
 }
